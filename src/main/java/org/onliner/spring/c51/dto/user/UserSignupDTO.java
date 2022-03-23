ee4a1383ad5fdc306716d5c5b18b4edeb32e7aad
papaslip <kuchuk.sa17@gmail.com>
2022-03-23 23:17:33 +0300

    converter test

--- src/main/java/org/onliner/spring/c51/dto/user/UserSignupDTO.java
+++ src/main/java/org/onliner/spring/c51/dto/user/UserSignupDTO.java
@@ -2,6 +2,8 @@ package org.onliner.spring.c51.dto.user;
 
 import org.hibernate.validator.constraints.Email;
 
+import java.util.Objects;
+
 public class UserSignupDTO {
 
     private String firstName;
@@ -53,5 +55,18 @@ public class UserSignupDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserSignupDTO that = (UserSignupDTO) o;
+        return Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(username, that.username) && Objects.equals(email, that.email) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, username, email, password);
     }
 }
---
+++ src/test/java/org/onliner/spring/c51/dtoconverter/impl/UserDTOConverterImplTest.java
@@ -0,0 +1,51 @@
+package org.onliner.spring.c51.dtoconverter.impl;
+
+import org.junit.jupiter.api.Test;
+import org.onliner.spring.c51.dto.user.UserSignupDTO;
+import org.onliner.spring.c51.dtoconverter.UserDTOConverter;
+import org.onliner.spring.c51.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDTOConverterImplTest {
+
+
+    private UserDTOConverter userDTOConverter = new UserDTOConverterImpl();
+
+    @Test
+    void convertToUserSignupDTOFromUser() {
+        String list[] = {"Serj","Kuchuk","Papaslip1212","skp@.gmail.com","123qwe"};
+        User user = new User();
+        user.setFirstName(list[0]);
+        user.setLastName(list[1]);
+        user.setUsername(list[2]);
+        user.setEmail(list[3]);
+        user.setPassword(list[4]);
+        UserSignupDTO userSignupDTO = new UserSignupDTO();
+        userSignupDTO.setFirstName(list[0]);
+        userSignupDTO.setLastName(list[1]);
+        userSignupDTO.setUsername(list[2]);
+        userSignupDTO.setEmail(list[3]);
+        userSignupDTO.setPassword(list[4]);
+        assertTrue(userSignupDTO.equals(userDTOConverter.convertToUserSignupDTOFromUser(user)));
+    }
+
+    @Test
+    void convertToUserFromUserSignupDTO() {
+        String list[] = {"Serj","Kuchuk","Papaslip1212","skp@.gmail.com","123qwe"};
+        User user = new User();
+        user.setFirstName(list[0]);
+        user.setLastName(list[1]);
+        user.setUsername(list[2]);
+        user.setEmail(list[3]);
+        user.setPassword(list[4]);
+        UserSignupDTO userSignupDTO = new UserSignupDTO();
+        userSignupDTO.setFirstName(list[0]);
+        userSignupDTO.setLastName(list[1]);
+        userSignupDTO.setUsername(list[2]);
+        userSignupDTO.setEmail(list[3]);
+        userSignupDTO.setPassword(list[4]);
+        assertTrue(user.equals(userDTOConverter.convertToUserFromUserSignupDTO(userSignupDTO)));
+    }
+}
