0d83e728d3de6a75edb316c167f7d847fe58a702
SergeyAnshin <anshin.serega@gmail.com>
2022-03-26 16:37:47 +0300

    added auth user dto

---
+++ src/main/java/org/onliner/spring/c51/dto/user/AuthenticatedUserDTO.java
@@ -0,0 +1,13 @@
+package org.onliner.spring.c51.dto.user;
+
+public class AuthenticatedUserDTO {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
