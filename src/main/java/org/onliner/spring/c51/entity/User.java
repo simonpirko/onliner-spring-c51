626110a4e2c33b73f5cc47b985407619434e52e4
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-03-30 19:58:01 +0300

    add lombok to User

--- src/main/java/org/onliner/spring/c51/entity/User.java
+++ src/main/java/org/onliner/spring/c51/entity/User.java
@@ -1,9 +1,6 @@
 package org.onliner.spring.c51.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -20,6 +17,7 @@ import java.util.Set;
 @Data                       // ONL-12 : lombok
 @NoArgsConstructor          // ONL-12 : lombok
 @AllArgsConstructor         // ONL-12 : lombok
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder                    // ONL-12 : lombok
 @Entity
 @Table(name = "users")
@@ -31,6 +29,7 @@ public class User {
     private String firstName;
     private String lastName;
     private String username;
+    @EqualsAndHashCode.Include
     private String email;
     private String password;
     @ManyToMany(cascade = {CascadeType.MERGE})
@@ -38,87 +37,6 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles = new HashSet<>();
-
-
-
-
-    public static class Builder {
-        private long builderId;
-        private String builderFirstName;
-        private String builderLastName;
-        private String builderUsername;
-        private String builderEmail;
-        private String builderPassword;
-        private Set<Role> builderRoles;
-
-        public Builder() {
-        }
-
-        public Builder id(long id) {
-            builderId = id;
-            return this;
-        }
-
-        public Builder firstName(String firstName) {
-            builderFirstName = firstName;
-            return this;
-        }
-
-        public Builder lastName(String lastName) {
-            builderLastName = lastName;
-            return this;
-        }
-
-        public Builder username(String username) {
-            builderUsername = username;
-            return this;
-        }
-
-        public Builder email(String email) {
-            builderEmail = email;
-            return this;
-        }
-
-        public Builder password(String password) {
-            builderPassword = password;
-            return this;
-        }
-
-        public Builder roles(Set<Role> roles) {
-            builderRoles = roles;
-            return this;
-        }
-
-        public User build() {
-            return new User(this);
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(email);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", roles=" + roles +
-                '}';
-    }
 
     public void addRole(Role role) {
         roles.add(role);
