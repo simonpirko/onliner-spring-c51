9bb6dbb9d88a8a2b6a7ea04aa843bcf944acc8a1
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-04-10 14:59:43 +0300

    add data to sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,9 @@ INSERT INTO users_roles(user_id, role_id) VALUE (2, 2);
 
 INSERT INTO smartphone(name, displayResolution, os, platform, ram, storage, price) VALUE ('Samsung Galaxy Z Flip3 5G 8GB/256GB (red)', 1, 2, 3, 4, 5, 12);
 INSERT INTO smartphone(name, displayResolution, os, platform, ram, storage, price) VALUE ('Samsung Galaxy Z Flip3 8G 16GB/500GB (green)', 1, 2, 3, 4, 5, 12);
+
+INSERT INTO state_registration_information(legalEntityName, stateRegistrationLegalEntityPlace, payerAccountNumber, seller) VALUE ('seller', 1, 666, 1);
+
+INSERT INTO seller(stateRegistrationInformation, verifiedSeller) VALUE (1, true);
+
+INSERT INTO sellerfeedback(title, description, grade, user, seller) VALUE ('test', 'test', 'test', 1, 1);
