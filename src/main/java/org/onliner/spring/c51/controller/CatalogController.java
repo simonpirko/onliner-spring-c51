e6885f761aa54bf5419e5b69bc80b6bc94dbfdcc
SergeyAnshin <anshin.serega@gmail.com>
2022-04-11 00:26:18 +0300

    changed catalog classes

--- src/main/java/org/onliner/spring/c51/controller/CatalogController.java
+++ src/main/java/org/onliner/spring/c51/controller/CatalogController.java
@@ -1,7 +1,8 @@
 package org.onliner.spring.c51.controller;
 
-import org.onliner.spring.c51.enums.ProductType;
+import org.onliner.spring.c51.enums.ProductTypes;
 import org.onliner.spring.c51.service.ProductService;
+import org.onliner.spring.c51.service.ProductTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,31 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/catalog")
 public class CatalogController {
     public static final String PATH_CATALOG_TEMPLATE = "catalog/catalog-page";
-    public static final String PATH_RADIOTELEPHONES_TEMPLATE = "catalog/radiotelephones";
     public static final String PATH_SMARTPHONES_TEMPLATE = "catalog/smartphones";
     public static final String ATTRIBUTE_CATALOG = "catalog";
     public static final String ATTRIBUTE_PRODUCTS = "products";
     private final ProductService productService;
+    private final ProductTypeService productTypeService;
 
-    public CatalogController(ProductService productService) {
+    public CatalogController(ProductService productService, ProductTypeService productTypeService) {
         this.productService = productService;
+        this.productTypeService = productTypeService;
     }
 
     @GetMapping
     public String getCatalogTemplate(Model model) {
-        model.addAttribute(ATTRIBUTE_CATALOG, productService.getCatalog());
+        model.addAttribute(ATTRIBUTE_CATALOG, productTypeService.getCatalog());
         return PATH_CATALOG_TEMPLATE;
-    }
-
-    @GetMapping("/radiotelephone")
-    public String getRadiotelephonesTemplate(Model model) {
-        model.addAttribute(ATTRIBUTE_PRODUCTS, productService.findAllByProductType(ProductType.RADIOTELEPHONE));
-        return PATH_RADIOTELEPHONES_TEMPLATE;
     }
 
     @GetMapping("/smartphone")
     public String getSmartphonesTemplate(Model model) {
-        model.addAttribute(ATTRIBUTE_PRODUCTS, productService.findAllByProductType(ProductType.SMARTPHONE));
+        model.addAttribute(ATTRIBUTE_PRODUCTS, productService.findAllByProductType(ProductTypes.SMARTPHONE));
         return PATH_SMARTPHONES_TEMPLATE;
     }
 }
