ef612c58d15cdddd273a94e9f855b8b251351c55
SergeyAnshin <anshin.serega@gmail.com>
2022-04-19 17:39:04 +0300

    added security

--- src/main/java/org/onliner/spring/c51/configuration/WebConfiguration.java
+++ src/main/java/org/onliner/spring/c51/configuration/WebConfiguration.java
@@ -2,6 +2,7 @@ package org.onliner.spring.c51.configuration;
 
 import org.onliner.spring.c51.interceptor.AdminRoleAuthorizationInterceptor;
 import org.onliner.spring.c51.interceptor.AuthenticationInterceptor;
+import org.onliner.spring.c51.interceptor.ManagerRoleAuthorizationInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,10 +15,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     private AuthenticationInterceptor authenticationInterceptor;
     private AdminRoleAuthorizationInterceptor adminRoleAuthorizationInterceptor;
+    private ManagerRoleAuthorizationInterceptor managerRoleAuthorizationInterceptor;
 
-    public WebConfiguration(AuthenticationInterceptor authenticationInterceptor, AdminRoleAuthorizationInterceptor adminRoleAuthorizationInterceptor) {
+    public WebConfiguration(AuthenticationInterceptor authenticationInterceptor, AdminRoleAuthorizationInterceptor adminRoleAuthorizationInterceptor,
+                            ManagerRoleAuthorizationInterceptor managerRoleAuthorizationInterceptor) {
         this.authenticationInterceptor = authenticationInterceptor;
         this.adminRoleAuthorizationInterceptor = adminRoleAuthorizationInterceptor;
+        this.managerRoleAuthorizationInterceptor = managerRoleAuthorizationInterceptor;
     }
 
     @Override
@@ -25,9 +29,14 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/user/**")
                     .excludePathPatterns("/user/signup", "/user/login", "/user/*")
-                .addPathPatterns("/admin/**");
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/product/**")
+                        .excludePathPatterns("/product/{manufacturer}/{id}");
 
         registry.addInterceptor(adminRoleAuthorizationInterceptor)
                 .addPathPatterns("/admin/**");
+
+        registry.addInterceptor(managerRoleAuthorizationInterceptor)
+                .addPathPatterns("/product/{id}/create-offer");
     }
 }
---
+++ src/main/java/org/onliner/spring/c51/interceptor/ManagerRoleAuthorizationInterceptor.java
@@ -0,0 +1,26 @@
+package org.onliner.spring.c51.interceptor;
+
+import org.onliner.spring.c51.dto.user.AuthenticatedUserDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.onliner.spring.c51.controller.UserController.ATTRIBUTE_AUTHENTICATED_USER;
+import static org.onliner.spring.c51.interceptor.AuthenticationInterceptor.URL_LOGIN_PAGE;
+
+@Component
+public class ManagerRoleAuthorizationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        AuthenticatedUserDTO authenticatedUser = (AuthenticatedUserDTO) request.getSession().getAttribute(ATTRIBUTE_AUTHENTICATED_USER);
+        if (authenticatedUser != null && authenticatedUser.isManager()) {
+            return true;
+        } else {
+            response.sendRedirect(URL_LOGIN_PAGE);
+            return false;
+        }
+    }
+}
