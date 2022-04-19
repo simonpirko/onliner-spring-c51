3b5b856a6cad8c0452ba290774596f6d19df93b5
SergeyAnshin <anshin.serega@gmail.com>
2022-04-19 17:42:03 +0300

    added seller search by employee

--- src/main/java/org/onliner/spring/c51/dao/SellerDAO.java
+++ src/main/java/org/onliner/spring/c51/dao/SellerDAO.java
@@ -2,5 +2,9 @@ package org.onliner.spring.c51.dao;
 
 import org.onliner.spring.c51.entity.Seller;
 
+import java.util.Optional;
+
 public interface SellerDAO extends GenericEntityDAO<Seller> {
+
+    Optional<Seller> findBySellerManagerId(long sellerManagerId);
 }
